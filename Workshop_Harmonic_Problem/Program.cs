7349eb9e043fcb6a5fd4745f2c1a8e223c8814b5
Praful <prafulrakhade02@gmail.com>
2022-06-07 09:53:25 +0530

    [praful] Added Workshop Harmonic Program File

---
+++ Workshop_Harmonic_Problem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Workshop_Harmonic_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------WELCOME TO THE HARMONIC PROBLEM-----------\n");
+            double num1;
+            double result = 0;
+            Console.WriteLine("Enter the value for the Harmonic value ");
+            num1 = double.Parse(Console.ReadLine());
+            for(double i=1; i<=num1;i++)
+            {
+                result = result + 1 / i;
+            }
+            Console.WriteLine("Harmonic Number of "+num1 + " is " + result);
+        }
+    }
+}
